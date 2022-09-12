df343b9f97f1b848a31d67415bdf1ecc2739725f
VictoriyaK19 <kalinovavictoriya@gmail.com>
2022-09-12 17:48:00 +0300

    second commit

--- src/FisrStepsInCoding/Exercise/Repainting.java
+++ src/FisrStepsInCoding/Exercise/Repainting.java
@@ -7,8 +7,15 @@ public class Repainting {
         Scanner scanner = new Scanner(System.in);
 
         int nylon = Integer.parseInt(scanner.nextLine());
+        int paint = Integer.parseInt(scanner.nextLine());
+        int diluent = Integer.parseInt(scanner.nextLine());
+        int hours = Integer.parseInt(scanner.nextLine());
 
-        System.out.println(nylon);
+        double sum = (nylon + 2) * 1.5 + paint * 1.1 * 14.5 + diluent * 5 + 0.4;
+        double sumCraft = (sum * 0.3) * hours;
+        double totalSum = sum + sumCraft;
+
+        System.out.println(totalSum);
 
     }
 }
