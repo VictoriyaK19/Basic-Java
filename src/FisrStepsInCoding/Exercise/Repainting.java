220e585cbb342a03eb6e0f0f4a22f680a28f5fd2
VictoriyaK19 <kalinovavictoriya@gmail.com>
2022-09-12 16:18:54 +0300

    second commit

--- src/FisrStepsInCoding/Exercise/Repainting.java
+++ src/FisrStepsInCoding/Exercise/Repainting.java
@@ -6,7 +6,9 @@ public class Repainting {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int nylon = Integer.parseInt(scanner.nextLine());
 
+        System.out.println(nylon);
 
     }
 }
