89eb1599b9026a78014062b28fc5a9d2a961148f
VictoriyaK19 <kalinovavictoriya@gmail.com>
2022-10-06 14:36:17 +0300

    commit

---
+++ src/ForLoop/Lab/NumbersN1.java
@@ -0,0 +1,18 @@
+package ForLoop.Lab;
+
+import java.util.Scanner;
+
+public class NumbersN1 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+
+        for (int i = number; i > 0; i--) {
+            System.out.println(i);
+        }
+
+    }
+
+}
