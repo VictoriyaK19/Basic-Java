3de9496e6586c6bc27e6d1fa169e33258225c983
VictoriyaK19 <kalinovavictoriya@gmail.com>
2022-10-06 14:31:15 +0300

    commit

---
+++ src/ForLoop/Lab/NumbersFrom1To100.java
@@ -0,0 +1,12 @@
+package ForLoop.Lab;
+
+
+public class NumbersFrom1To100 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 101; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
