1b6e689545864448e284322a2fff27223a8202ce
VictoriyaK19 <kalinovavictoriya@gmail.com>
2022-10-09 11:36:19 +0300

    commit

---
+++ src/WhileLoop/Lab/Password.java
@@ -0,0 +1,20 @@
+package WhileLoop.Lab;
+
+import java.util.Scanner;
+
+public class Password {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String username = scanner.nextLine();
+        String password = scanner.nextLine();
+        String input = scanner.nextLine();
+
+        while (!password.equals(input)) {
+            input = scanner.nextLine();
+        }
+
+        System.out.println("Welcome " + username + "!");
+
+    }
+}
