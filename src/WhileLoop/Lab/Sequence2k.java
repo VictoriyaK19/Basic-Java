af7b12a2483e9e84b5eee817fc2970156d016140
VictoriyaK19 <kalinovavictoriya@gmail.com>
2022-10-09 11:56:35 +0300

    commit

---
+++ src/WhileLoop/Lab/Sequence2k.java
@@ -0,0 +1,18 @@
+package WhileLoop.Lab;
+
+import java.util.Scanner;
+
+public class Sequence2k {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num = Integer.parseInt(scanner.nextLine());
+        int sum = 1;
+
+        while (sum <= num) {
+            System.out.println(sum);
+            sum = sum * 2 + 1;
+        }
+
+    }
+}
